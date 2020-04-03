9676a1bc65983e03471f304a740185e74feebb3b
leeejin <asd72621@naver.com>
2020-04-03 12:34:28 +0900

    input사용

---
+++ Assignment_2weeks/input.py
@@ -0,0 +1,4 @@
+x = input("첫 번째 정수: ")
+y = input("두 번째 정수: ")
+sum = x+y
+print("합은 ",sum)
---
+++ Assignment_2weeks/input_Number.py
@@ -0,0 +1,5 @@
+width = int(input("밑면의 길이는: "))
+height = int(input("높이의 길이는: "))
+area = width * height
+perimeter =(width + height)
+print("직사각형의 면적은",area,"둘레는",perimeter)
