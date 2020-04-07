531ae3d4cc2af42e0825d981fdbd99444c0562ac
leeejin <asd72621@naver.com>
2020-04-07 23:37:55 +0900

    원그리기

---
+++ Assignment_2weeks/circle1.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Turtle()
+t.circle(100)
+t.penup()
+t.circle(100, extent = 60)
+t.pendown()
+t.circle(100, steps = 3)
---
+++ Assignment_2weeks/circle2.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Turtle()
+t.circle(100)
+t.penup()
+t.circle(100, extent = 108)
+t.pendown()
+t.circle(100, steps = 5)
